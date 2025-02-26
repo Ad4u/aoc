c6b522c5919ce8ba6474196be81144d0297eef07
Adrien Aubourg <adrien.aubourg@gmail.com>
2025-02-26 22:31:09 +0100

    Overall benchmark

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -5,6 +5,10 @@ zig build -Doptimize=ReleaseFast
 SOLVERS="$(ls -m ./src/solvers/ | tr -d '.zig \n')"
 
 hyperfine -w 50 -M 100 \
---export-csv benchmark/bench.csv \
+--export-csv benchmark/solvers.csv \
 --sort command \
 -L solvers "$SOLVERS" './zig-out/bin/aoc {solvers}' 2> /dev/null
+
+hyperfine -w 50 \
+--export-csv benchmark/total.csv \
+'./zig-out/bin/aoc'
