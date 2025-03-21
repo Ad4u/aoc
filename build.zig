4ca1cdbbf36419eb8da68674c9b4898bcc348933
Adrien Aubourg <adrien.aubourg@gmail.com>
2025-03-21 11:02:19 +0100

    Add check step for zls + use root_module

--- build.zig
+++ build.zig
@@ -4,11 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "aoc",
+    const main_module = b.addModule("aoc", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "aoc",
+        .root_module = main_module,
     });
     b.installArtifact(exe);
 
@@ -23,9 +27,16 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const test_step = b.step("test", "Run unit tests");
-    const exe_unit_tests = b.addTest(.{ .root_source_file = b.path("src/main.zig") });
+    const exe_unit_tests = b.addTest(.{ .root_module = main_module });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const exe_check = b.addExecutable(.{
+        .name = "aoc",
+        .root_module = main_module,
+    });
+    const check = b.step("check", "Check if code compiles");
+    check.dependOn(&exe_check.step);
 }
 
 fn fatal(err: anyerror) noreturn {
